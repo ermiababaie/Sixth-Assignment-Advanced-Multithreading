b0d52244d109b282d9dca87fc830f238ec790a0b
ermiababaie <ermiababaieam@yahoo.com>
2024-05-18 16:48:47 +0330

    semaphore is done.

--- src/main/java/sbu/cs/Semaphore/Operator.java
+++ src/main/java/sbu/cs/Semaphore/Operator.java
@@ -1,16 +1,30 @@
 package sbu.cs.Semaphore;
 
-public class Operator extends Thread {
+import java.util.concurrent.Semaphore;
 
-    public Operator(String name) {
+public class Operator extends Thread {
+    Semaphore semaphore;
+    public Operator(String name, Semaphore semaphore) {
         super(name);
+        this.semaphore = semaphore;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < 10; i++)
         {
-            Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
+            try {
+                semaphore.acquire();
+                System.out.println(getName());
+                Resource.accessResource();
+            }
+            catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+            finally {
+                semaphore.release();
+            }
+                   // critical section - a Maximum of 2 operators can access the resource concurrently
             try {
                 sleep(500);
             } catch (InterruptedException e) {
